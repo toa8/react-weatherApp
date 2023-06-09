f687931a42d73b6506c8da2f8cd008bb98cdc10d
Talha Orhun Akdas <talhaorhunakdas@gmail.com>
2023-06-09 15:29:44 +0300

    style: main page designed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 const App = () => {
-  return <section className="app"></section>;
+  return (
+    <section className="app">
+      <div className="card">
+        <p className="title">How is weather right now?</p>
+        <form className="form">
+          <input
+            type="text"
+            placeholder="Enter a Country/City name"
+            maxLength={40}
+          />
+          <button>Search</button>
+        </form>
+        <span className="line"></span>
+        <div className="result"></div>
+      </div>
+    </section>
+  );
 };
 
 export default App;
